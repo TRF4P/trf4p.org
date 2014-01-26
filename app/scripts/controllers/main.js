76763fc04bffad7370a33e35359c0dc726061d27
clarknoah <noahb08@gmail.com>
2014-01-26 07:21:52 -0500

    hehe

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('trf4porgApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
